baa5d483019836ae1e79e482bc36e53b6a5e3a6b
kasnder <5175206+kasnder@users.noreply.github.com>
2020-06-11 17:39:09 +0000

    Working on retrieving IAP, Ads, Kids attributes

---
+++ pipeline/archiver/retriever/retrieve_iap.js
@@ -0,0 +1,72 @@
+const logger = require('../../util/logger');
+
+const region = 'gb';
+
+let db = new (require('../../db/db'))('retriever');
+let retriever = require('google-play-scraper');
+let throttle = 10;
+let timeout = 10 * 1000;
+
+let notFound = [];
+
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+(async() => {
+    while(true) {
+        const batch = 1000;
+        const res = await db.query(
+            'SELECT a.id, app, store_url \
+             FROM playstore_apps b \
+             JOIN app_versions a ON a.id = b.id \
+             WHERE offersIAP IS NULL AND date_part(\'year\', released) > 2000 \
+             ORDER BY updated DESC \
+             LIMIT $1 ', [batch]);
+        if (res.rowCount <= 0) {
+            logger.info('No more apps to process. Exiting..');
+            break;
+        }
+        logger.info('Found apps to process:', res.rowCount);
+
+        try {
+            await Promise.all( res.rows.map( app => {
+                let id = app.app;
+                if (notFound.includes(id))
+                    return;
+
+                return retriever.app({
+                    appId: id, 
+                    throttle: throttle,
+                    country: region
+                }).then(async(appData) => {
+                    if (appData.offersIAP !== undefined) {                                
+                        logger.debug(`updating ${appData.title} with ${appData.offersIAP} in DB`);
+                        await db.query(
+                            'UPDATE playstore_apps \
+                             SET offersIAP = $1 \
+                             WHERE id = $2',
+                             [appData.offersIAP, app.id]
+                        );
+                    } else {
+                        logger.debug(`nothing to update for ${appData.title} in DB`);
+                    }
+                },
+                async (err) => {
+                    logger.err(`Error Requesting processing: ${app.app} (${id}). Error: ${err}`);
+                    if (err.message.includes('App not found (404)')) {
+                        notFound.append(app.app);
+                        logger.err('APP NOT FOUND 404');
+                    } else {
+                        throw err;
+                    }
+                });
+            }))
+        } catch(err) {
+            logger.debug(`pausing due to error while processing: ${err}`);
+            await sleep(timeout);
+        }
+    }
+})();
